c43096cd69805b3a40094bc04f4097af5b1b35c9
adolfosonchiche <sonadolfo36@gmail.com>
2022-01-01 19:24:22 -0600

    color fondo

--- reliquias-sc/src/pages/home/index.js
+++ reliquias-sc/src/pages/home/index.js
@@ -1,37 +1,53 @@
-import { Col, CardBody, CardTitle, CardText } from 'reactstrap';
-import { Text } from 'react-native';
+import { Col, CardBody, Container, CardTitle, CardText } from 'reactstrap';
 
 
 
 import IconFace from '../../resource/facebook.png';
 import IconTwitter from '../../resource/twitter.png';
 import IconGit from '../../resource/git.png';
+import IconNerwork from '../../resource/network.png'
+import IconLocal from '../../resource/local.png'
+import IconServer from '../../resource/server.png'
 
 const inicio = () => {
 	const title = ' Adolfo Son Chiché';
 	const content = 'Bienvenido al sitio web de';
-	let urlFacebook = 'https://www.facebook.com/Ad.395'
-	let urlTwitter = 'https://twitter.com/sc_ado'
-	let urlGit = 'https://github.com/adolfosonchiche'
+	const description = 'Estudiante de Ingeniería en Ciencias y Sistemas'
+	const desU = 'USAC'
+	 const urlFacebook = 'https://www.facebook.com/Ad.395'
+	const urlTwitter = 'https://twitter.com/sc_ado'
+	const urlGit = 'https://github.com/adolfosonchiche'
 
 
 
 	return (
 		<Col xs="12">
-			<Col>
-				<Text>contacto:</Text>
+			<Container>
 				<a href={urlFacebook}><img  top width="5%" height="" src={IconFace} alt="facebook" /></a>
 				<a href={urlTwitter}><img top width="5%" height="" src={IconTwitter} alt="facebook" /></a>
 				<a href={urlGit}><img top width="6%" height="" src={IconGit} alt="facebook" /></a>
-			</Col>
+			</Container>
 
 			<br />
-			<Col style={{ backgroundColor: '#78bfdf', opacity: '0.6' }}>
+			<Container style={{ backgroundColor: '#78bfdf', opacity: '0.6' }}>
 				<CardBody>
 					<CardText>{content}</CardText>
-					<CardTitle tag="h5">{title}</CardTitle>
+					<CardTitle tag="h4">{title}</CardTitle>
 				</CardBody>
-			</Col>
+			</Container>
+			<br/>
+			<Container style={{ backgroundColor: '#51e9a9', opacity: '0.6' }}>
+				<CardBody>
+					<CardTitle tag="h6">{description}</CardTitle>
+					<CardText>{desU}</CardText>
+				</CardBody>
+				<Col>
+				<img  top width="5%" height="" src={IconServer} alt="" />
+				<img  top width="5%" height="" src={IconLocal} alt="" />
+				<img  top width="5%" height="" src={IconNerwork} alt="" />
+				<br/>
+				</Col>
+			</Container>
 		</Col>
 	);
 };
